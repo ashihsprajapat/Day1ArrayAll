eb300f5c2e67630d638c7e2accc375a0d4ecd613
ashish prajapat <ashishprajapat507@gmail.com>
2024-09-09 20:55:15 +0530

    initilization to arr

--- IntroToArray.java
+++ IntroToArray.java
@@ -3,6 +3,19 @@ package ArrayDay1;
 class IntroToArray {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        // initilization to array
+        int arr[] = new int[3];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 2;
+        }
+
+        int arr2[] = { 2, 5, 7, 1 };
+        int arr3[] = new int[arr.length];
+        for (int i : arr2) {
+            arr3[i] = arr2[i];
+        }
+        for (int i : arr3) {
+            System.out.println(i);
+        }
     }
 }
